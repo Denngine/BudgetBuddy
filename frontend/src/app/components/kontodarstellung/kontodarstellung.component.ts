4fd5c48ffe3b902a847f7bd734b46092640eaca9
Dennis <denngine@gmail.com>
2024-04-16 13:55:26 +0200

    alles neu

---
+++ frontend/src/app/components/kontodarstellung/kontodarstellung.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kontodarstellung',
+  templateUrl: './kontodarstellung.component.html',
+  styleUrl: './kontodarstellung.component.scss'
+})
+export class KontodarstellungComponent {
+
+}
---
+++ haushaltsbuch/src/app/components/menats-tabelle/menats-tabelle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menats-tabelle',
+  templateUrl: './menats-tabelle.component.html',
+  styleUrl: './menats-tabelle.component.scss'
+})
+export class MenatsTabelleComponent {
+
+}
